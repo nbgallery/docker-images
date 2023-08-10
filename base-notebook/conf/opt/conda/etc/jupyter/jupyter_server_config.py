929a6d1b53a36bd3172988dbff57a5545e6b6fc2
mcrutch <51446717+mcrutch@users.noreply.github.com>
2023-08-10 17:32:10 +0000

    Fixing logging errors on startup

--- base-notebook/conf/opt/conda/etc/jupyter/jupyter_server_config.py
+++ base-notebook/conf/opt/conda/etc/jupyter/jupyter_server_config.py
@@ -21,23 +21,23 @@ def find_nbgallery_url():
     return os.getenv('NBGALLERY_URL', 'http://localhost:3000')
 
 # Jupyter settings
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.open_browser = False
-c.NotebookApp.reraise_server_extension_failures = True
+c.ServerApp.ip = '0.0.0.0'
+c.ExtensionApp.open_browser = False
+c.ServerApp.reraise_server_extension_failures = True
 
 # Notebook directory can be mounted in, or it will be created by startup hook
 notebook_dir = os.getenv('NOTEBOOK_DIR')
-c.NotebookApp.notebook_dir = notebook_dir
+c.ServerApp.root_dir = notebook_dir
 
 # Security settings required to receive notebooks from nbgallery
 nbgallery_url = find_nbgallery_url()
 # To support multiple nbgalleries, use allow_origin_pat = 'regex'
-c.NotebookApp.allow_origin = nbgallery_url
-c.NotebookApp.allow_credentials = True
-c.NotebookApp.disable_check_xsrf = True
+c.ServerApp.allow_origin = nbgallery_url
+c.ServerApp.allow_credentials = True
+c.ServerApp.disable_check_xsrf = True
 
 # Set SameSite cookie options when running secure.
 # Ideally we should check if certfile is set, but within this file
 # there doesn't seem to be a way to check if it was set elsewhere.
 if 'GEN_CERT' in os.environ:
-    c.NotebookApp.cookie_options = {"SameSite": "None", "Secure": True}
+    c.ServerApp.cookie_options = {"SameSite": "None", "Secure": True}
